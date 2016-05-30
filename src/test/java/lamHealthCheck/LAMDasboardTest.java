9ef982715c1e6e3b30662399a3506ba918c2f0e3
shivani gupta <shivani.gupta@macys.com>
2016-05-30 19:28:14 +0530

    IE focus code added

--- src/test/java/lamHealthCheck/LAMDasboardTest.java
+++ src/test/java/lamHealthCheck/LAMDasboardTest.java
@@ -86,7 +86,7 @@ public class LAMDasboardTest {
 		StringSelection selec= new StringSelection(username);
 		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 		clipboard.setContents(selec, null);
-		Thread.sleep(10000);		
+		Thread.sleep(7000);		
 		robot.keyPress(KeyEvent.VK_CONTROL);
 		robot.keyPress(KeyEvent.VK_V);
 		Thread.sleep(1000);
