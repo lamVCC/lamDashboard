1d3364570800979fd08b374948918842f7e41fa9
akash.gupta <akash.gupta@gspann.com>
2016-06-03 15:07:52 +0530

    code added

--- src/test/java/lamHealthCheck/LAMDasboardTest.java
+++ src/test/java/lamHealthCheck/LAMDasboardTest.java
@@ -78,10 +78,19 @@ public class LAMDasboardTest {
 	public void enterCredentialsInHTTPAuthentication(String username,String password) throws AWTException, InterruptedException{
 		Thread.sleep(2000);
 		Robot robot = new Robot();
+		
+		robot.keyPress(KeyEvent.VK_WINDOWS);
+		robot.keyPress(KeyEvent.VK_D);
+		Thread.sleep(100);
+		robot.keyRelease(KeyEvent.VK_WINDOWS);
+		robot.keyRelease(KeyEvent.VK_D);
+		Thread.sleep(100);
 		robot.keyPress(KeyEvent.VK_WINDOWS);
 		robot.keyPress(KeyEvent.VK_TAB);
+		Thread.sleep(100);
 		robot.keyRelease(KeyEvent.VK_TAB);
 		robot.keyRelease(KeyEvent.VK_WINDOWS);
+		
 		StringSelection selec= new StringSelection(username);
 		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 		clipboard.setContents(selec, null);
